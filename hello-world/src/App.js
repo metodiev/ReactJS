c31df85e66ceeebe68ca4c99fb0b29127d46fad9
metodiev <miroslav.l.metodiev@gmail.com>
2022-06-01 21:14:29 +0300

    add Destruccting props and state

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,28 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import {Greet}
- from './components/Greet';
-import { render } from "@testing-library/react";
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
+
 import Counter from './components/Counter';
+import GreetComponent from './components/GreetComponent';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-   
-     <Counter/>
-      Create Statless Functional component 
-     
-     <Greet name="Bruse" heroName="Batman">
-       <p>This is children props</p>
-      </Greet>
-      <button>Action</button>
-      <Greet name="Clark" heroName="Superman"/>
-      <Greet name="Diana" heroName="Wonder Woman"/>
-      <Welcome/>
-      <Hello/>
+      <GreetComponent name="Miroslav" heroName="Miro"/>
+      <Welcome  name="Miroslav" heroName="Miro"/>
+   <Counter/>
     </div>
   );
 }
--- hello-world/src/components/Counter.js
+++ hello-world/src/components/Counter.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
 
+
+
 class Counter extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count: 0
+    }
+  }
+
+  increment () {
+    this.setState ({
+      count: this.state.count + 1
+    }, () => {console.log('Callback value', this.state.count)})
+   // this.state.count = this.state.count + 1
+    console.log(this.state.count)
+  }
+
+  incrementFive() {
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+  }
+
+  incrementCorrectFive() {
+    this.setState((prevState, props) => ({
+      count: prevState.count +1
+    })) 
+    console.log(this.state.count)
+  }
+
+  incrementTheCurrentStateWithFive(){
+    this.incrementCorrectFive()
+    this.incrementCorrectFive()
+    this.incrementCorrectFive()
+    this.incrementCorrectFive()
+    this.incrementCorrectFive()
+  }
+
   render() {
     return (
-      <div>Counter</div>
+      <div>
+        <div>Count - {this.state.count} </div>
+        <button onClick={() => this.incrementTheCurrentStateWithFive()}>Increment</button>
+      </div>
     )
   }
 }
